52363aa543f7e3a7cf5b6fe7128ab29e1895701c
chaitak-gorai <chaitakgorai@gmail.com>
2022-09-08 18:52:52 +0530

    Password change

--- src/pages/account.js
+++ src/pages/account.js
@@ -1,5 +1,16 @@
 import Head from "next/head";
-import { Box, Container, Grid, Typography } from "@mui/material";
+import {
+  Alert,
+  Box,
+  Button,
+  Card,
+  CardContent,
+  Container,
+  Divider,
+  Grid,
+  TextField,
+  Typography,
+} from "@mui/material";
 import { AccountProfile } from "../components/account/account-profile";
 import { AccountProfileDetails } from "../components/account/account-profile-details";
 import { Support } from "../components/account/support";
@@ -13,7 +24,8 @@ import { useEffect, useState } from "react";
 import ToggleButton from "@mui/material/ToggleButton";
 import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
 import axios from "axios";
-
+import { useFormik } from "formik";
+import * as Yup from "yup";
 const Account = () => {
   const router = useRouter();
   const dispatch = useDispatch();
@@ -23,6 +35,34 @@ const Account = () => {
   const [privacy, setPrivacy] = useState("");
   const [about, setAbout] = useState("");
   const [walletAmount, setWalletAmount] = useState(0);
+  const [msg, setMsg] = useState("");
+  const formik = useFormik({
+    initialValues: {
+      message: "",
+      phoneNo: "",
+    },
+    validationSchema: Yup.object({
+      message: Yup.string().max(255).required("Message is required"),
+      phoneNo: Yup.string().required("Phone No is required"),
+    }),
+    onSubmit: async () => {
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${userInfo.token}`,
+        },
+      };
+      const { data } = await axios.post(
+        `https://gravitybites.in/api/stores/support`,
+        {
+          message: formik.values.message,
+          phoneNo: formik.values.phoneNo,
+        },
+        config
+      );
+      setMsg(data.mess);
+    },
+  });
   useEffect(() => {
     if (!userInfo) {
       router.push("/login");
@@ -103,9 +143,72 @@ const Account = () => {
           <Typography sx={{ mb: 3 }} variant="h4">
             Support
           </Typography>
+          {msg != "" ? <Alert severity="success">{msg}</Alert> : ""}
           <Grid container spacing={3}>
             <Grid item lg={12} md={6} xs={12}>
-              <Support />
+              <Box
+                component="main"
+                sx={{
+                  alignItems: "center",
+                  display: "flex",
+                  flexGrow: 1,
+                  minHeight: "100%",
+                }}
+              >
+                <Container maxWidth="lg">
+                  <form onSubmit={formik.handleSubmit}>
+                    <Card>
+                      <Divider />
+                      <CardContent>
+                        <Grid container spacing={3}>
+                          <Grid item md={6} xs={12}>
+                            <TextField
+                              error={Boolean(formik.touched.message && formik.errors.message)}
+                              fullWidth
+                              helpertext={formik.touched.message && formik.errors.message}
+                              label="Message"
+                              margin="normal"
+                              name="message"
+                              onBlur={formik.handleBlur}
+                              onChange={formik.handleChange}
+                              value={formik.values.message}
+                              variant="outlined"
+                            />
+                          </Grid>
+
+                          <Grid item md={6} xs={12}>
+                            <TextField
+                              error={Boolean(formik.touched.phoneNo && formik.errors.phoneNo)}
+                              fullWidth
+                              helpertext={formik.touched.phoneNo && formik.errors.phoneNo}
+                              label="Phone No"
+                              margin="normal"
+                              name="phoneNo"
+                              onBlur={formik.handleBlur}
+                              onChange={formik.handleChange}
+                              value={formik.values.phoneNo}
+                              variant="outlined"
+                            />
+                          </Grid>
+                        </Grid>
+                      </CardContent>
+                      <Divider />
+                      <Box sx={{ py: 2 }}>
+                        <Button
+                          color="primary"
+                          disabled={formik.isSubmitting}
+                          fullWidth
+                          size="large"
+                          type="submit"
+                          variant="contained"
+                        >
+                          Support
+                        </Button>
+                      </Box>
+                    </Card>
+                  </form>
+                </Container>
+              </Box>
             </Grid>
           </Grid>
         </Container>
