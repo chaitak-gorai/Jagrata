295031a079c409c64dcedb2c43b833e72fa62e49
Pritam Das <pritamstudy123@gmail.com>
2022-09-09 19:32:31 +0530

    add product

--- src/components/product/product-list-toolbar.js
+++ src/components/product/product-list-toolbar.js
@@ -8,11 +8,28 @@ import {
   SvgIcon,
   Typography,
 } from "@mui/material";
+import Modal from "@mui/material/Modal";
+import * as React from "react";
 import { Download as DownloadIcon } from "../../icons/download";
 import { Search as SearchIcon } from "../../icons/search";
 import { Upload as UploadIcon } from "../../icons/upload";
+import ProductInput from "../addProductInput";
+export const ProductListToolbar = (props) => {
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
 
-export const ProductListToolbar = (props) => (
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
+  };
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  return(
   <Box {...props}>
     <Box
       sx={{
@@ -27,10 +44,20 @@ export const ProductListToolbar = (props) => (
         Products
       </Typography>
       <Box sx={{ m: 1 }}>
-        <Button color="primary" variant="contained">
+        <Button color="primary" variant="contained" onClick={handleOpen}>
           Add products
         </Button>
+        <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box sx={style}>
+              <ProductInput onclose={handleClose} />
+            </Box>
+          </Modal>
       </Box>
     </Box>
-  </Box>
-);
+  </Box>)
+};
