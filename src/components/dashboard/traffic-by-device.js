7d5cfc2b86e521fac6fe54ea307bd01ba2568e27
chaitak-gorai <chaitakgorai@gmail.com>
2022-09-12 20:51:29 +0530

    Dashboard Updated

--- src/components/dashboard/traffic-by-device.js
+++ src/components/dashboard/traffic-by-device.js
@@ -1,8 +1,8 @@
-import { Doughnut } from 'react-chartjs-2';
-import { Box, Card, CardContent, CardHeader, Divider, Typography, useTheme } from '@mui/material';
-import LaptopMacIcon from '@mui/icons-material/LaptopMac';
-import PhoneIcon from '@mui/icons-material/Phone';
-import TabletIcon from '@mui/icons-material/Tablet';
+import { Doughnut } from "react-chartjs-2";
+import { Box, Card, CardContent, CardHeader, Divider, Typography, useTheme } from "@mui/material";
+import LaptopMacIcon from "@mui/icons-material/LaptopMac";
+import PhoneIcon from "@mui/icons-material/Phone";
+import TabletIcon from "@mui/icons-material/Tablet";
 
 export const TrafficByDevice = (props) => {
   const theme = useTheme();
@@ -11,13 +11,13 @@ export const TrafficByDevice = (props) => {
     datasets: [
       {
         data: [63, 15, 22],
-        backgroundColor: ['#3F51B5', '#e53935', '#FB8C00'],
+        backgroundColor: ["#3F51B5", "#e53935", "#FB8C00"],
         borderWidth: 8,
-        borderColor: '#FFFFFF',
-        hoverBorderColor: '#FFFFFF'
-      }
+        borderColor: "#FFFFFF",
+        hoverBorderColor: "#FFFFFF",
+      },
     ],
-    labels: ['Desktop', 'Tablet', 'Mobile']
+    labels: ["Product", "Orders", "Coupons"],
   };
 
   const options = {
@@ -25,7 +25,7 @@ export const TrafficByDevice = (props) => {
     cutoutPercentage: 80,
     layout: { padding: 0 },
     legend: {
-      display: false
+      display: false,
     },
     maintainAspectRatio: false,
     responsive: true,
@@ -37,81 +37,66 @@ export const TrafficByDevice = (props) => {
       enabled: true,
       footerFontColor: theme.palette.text.secondary,
       intersect: false,
-      mode: 'index',
-      titleFontColor: theme.palette.text.primary
-    }
+      mode: "index",
+      titleFontColor: theme.palette.text.primary,
+    },
   };
 
   const devices = [
     {
-      title: 'Desktop',
-      value: 63,
+      title: "Product",
+      value: 10,
       icon: LaptopMacIcon,
-      color: '#3F51B5'
+      color: "#3F51B5",
     },
     {
-      title: 'Tablet',
-      value: 15,
+      title: "Orders",
+      value: 4,
       icon: TabletIcon,
-      color: '#E53935'
+      color: "#E53935",
     },
     {
-      title: 'Mobile',
-      value: 23,
+      title: "Coupons",
+      value: 8,
       icon: PhoneIcon,
-      color: '#FB8C00'
-    }
+      color: "#FB8C00",
+    },
   ];
 
   return (
     <Card {...props}>
-      <CardHeader title="Traffic by Device" />
+      <CardHeader title="Data pie graph" />
       <Divider />
       <CardContent>
         <Box
           sx={{
             height: 300,
-            position: 'relative'
+            position: "relative",
           }}
         >
-          <Doughnut
-            data={data}
-            options={options}
-          />
+          <Doughnut data={data} options={options} />
         </Box>
         <Box
           sx={{
-            display: 'flex',
-            justifyContent: 'center',
-            pt: 2
+            display: "flex",
+            justifyContent: "center",
+            pt: 2,
           }}
         >
-          {devices.map(({
-            color,
-            icon: Icon,
-            title,
-            value
-          }) => (
+          {devices.map(({ color, icon: Icon, title, value }) => (
             <Box
               key={title}
               sx={{
                 p: 1,
-                textAlign: 'center'
+                textAlign: "center",
               }}
             >
               <Icon color="action" />
-              <Typography
-                color="textPrimary"
-                variant="body1"
-              >
+              <Typography color="textPrimary" variant="body1">
                 {title}
               </Typography>
-              <Typography
-                style={{ color }}
-                variant="h4"
-              >
-                {value}
-                %
+              <Typography style={{ color }} variant="h4">
+                {value}%
               </Typography>
             </Box>
           ))}
--- src/pages/index.js
+++ src/pages/index.js
@@ -91,17 +91,16 @@ const Dashboard = () => {
             <Grid item xl={3} lg={3} sm={6} xs={12}>
               <Sales />
             </Grid>
-
-            <Grid item lg={4} md={6} xl={3} xs={12}>
-              <TrafficByDevice sx={{ height: "100%" }} />
+            <Grid item lg={8} md={12} xl={9} xs={12}>
+              <LatestOrders orders={orders} />
             </Grid>
-          </Grid>
-          <Grid container spacing={3}>
+
             <Grid item lg={4} md={6} xl={3} xs={12}>
               <LatestProducts sx={{ height: "100%" }} products={products} />
             </Grid>
-            <Grid item lg={8} md={12} xl={9} xs={12}>
-              <LatestOrders orders={orders} />
+
+            <Grid item lg={4} md={6} xl={3} xs={12}>
+              <TrafficByDevice sx={{ height: "100%" }} />
             </Grid>
           </Grid>
         </Container>
